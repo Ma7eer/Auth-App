cbd2202dd11357dae66ecc4a1af04bce2f001b6e
Ma7eer <maher.alkendi2@gmail.com>
2018-09-06 04:54:45 -0600

    added ejs bodyparser and bulma

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# express-ejs-bodyparser-firebase-boilderplate
+# express-ejs-bodyparser-bulma-boilderplate
 A boilerplate to get you started with building an express application
--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-// TODO: setup ejs
-const express = require('express');
-const app = express();
+const express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+/* use static files */
+app.use(express.static(__dirname + '/public'));
+
+/* set view engine to ejs */
+app.set('view engine', 'ejs');
 
 /* PORT is set on package.json file under config */
-const PORT = process.env.npm_package_config_port;
+const PORT = process.env.npm_package_config_port || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  let world = 'world!'
+  res.render('pages/home', {
+    world: world
+  });
 })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
